ad7fa637c4b559b07e4365b0cc11060da6560751
Aditya Ishan <92012598+ggmxip@users.noreply.github.com>
2026-04-06 05:05:12 +0530

    first commit

---
+++ 01_setup/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// main package "main()" is the entry point of the application
+// go file looks for the package "main" and the function "main()" to start execution
+func main() {
+
+	fmt.Println("aaj kamainga toh kal gahr baith kar khainga")
+
+}
---
+++ 02_varable+types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name type
+	var name string
+	name = "Aditya"
+	fmt.Println("my name is", name)
+	// name is a variable of type string and it holds the value "Aditya"
+
+	var age int
+	age = 22
+	fmt.Println("my age is", age)
+
+	var marks float64
+	marks = 85.5
+	fmt.Println("my marks are", marks)
+}
