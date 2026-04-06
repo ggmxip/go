e0a2aeb587eca5afd43673559eca4b5f6d9a6433
Aditya Ishan <92012598+ggmxip@users.noreply.github.com>
2026-04-07 02:07:08 +0530

    just a revision test

--- 01_setup/main.go
+++ 01_setup/main.go
@@ -1,11 +1,21 @@
+// package main
+
+// import "fmt"
+
+// // main package "main()" is the entry point of the application
+// // go file looks for the package "main" and the function "main()" to start execution
+// func main() {
+
+// 	fmt.Println("aaj kamainga toh kal gahr baith kar khainga")
+
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// main package "main()" is the entry point of the application
-// go file looks for the package "main" and the function "main()" to start execution
 func main() {
-
-	fmt.Println("aaj kamainga toh kal gahr baith kar khainga")
-
+	fmt.Println("yes i can do this")
 }
---
+++ 03_package_imp/main.go
@@ -0,0 +1,15 @@
+package main
+
+// importing the fmt package to use the Println function
+
+import (
+	"fmt"
+	"math"
+	// importing the math package to use the Sqrt function
+)
+
+func main() {
+	//package.Function()
+	fmt.Println("sqrroot of 16 is", math.Sqrt(16))
+
+}
